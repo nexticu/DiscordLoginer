c4413014c420dca04808512c17eb18f7efa080a8
nexticu <152704191+nexticu@users.noreply.github.com>
2024-01-20 01:15:27 +0300

    Update Tokenloginer.meta.js

--- Tokenloginer.meta.js
+++ Tokenloginer.meta.js
@@ -1,1 +1,12 @@
-
+// ==UserScript==
+// @name         TokenLoging
+// @namespace    https://discord.com/invite/zPxF4DPnrY
+// @version      1.0
+// @description
+// @author       000sunray000
+// @match        *://discord.com/login
+// @icon         https://discord.com/assets/images/favicon.ico
+// @downloadURL  https://raw.githubusercontent.com/nexticu/DiscordLoginer/main/Tokenloginer.js
+// @updateURL    https://raw.githubusercontent.com/nexticu/DiscordLoginer/main/Tokenloginer.meta.js
+// @grant        none
+// ==/UserScript==
